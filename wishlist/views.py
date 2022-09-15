b555e8c4a99d7763476a1f5e3fee4c225a1f17e4
triviaspace <shafatriviaa@gmail.com>
2022-09-15 13:55:11 +0700

    data delivery

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,34 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
+
 # Create your views here.
 def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'safa'
+    }
     return render(request, "wishlist.html", context)
+    
 
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'safa'
-}
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    # Jika JSON
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+#def show_xml_by_id(request,id):
+    #data = BarangWishlist.objects.filter(pk=id)
+    # Jika XML
+    #return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
