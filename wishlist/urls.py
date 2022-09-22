c166914b696b19df64ac4c7d4815212c4f9905d7
triviaspace <shafatriviaa@gmail.com>
2022-09-22 13:25:21 +0700

    Form, Authentication, Session, dan Cookie

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,7 +3,10 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json_by_id
-#from wishlist.views import show_xml_by_id
+from wishlist.views import show_xml_by_id
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'wishlist'
 
@@ -12,5 +15,8 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'), 
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
-    #path('xml/<int:id>', show_json_by_id, name='show_xml_by_id'), 
+    path('xml/<int:id>', show_json_by_id, name='show_xml_by_id'), 
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
 ] 
